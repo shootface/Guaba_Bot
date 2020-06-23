df6c5599f693e558449f97abef6e7244b0657c07
shootface <31258676+shootface@users.noreply.github.com>
2020-06-22 20:08:05 -0500

    .gitignore fix

--- events/disconnet.js
+++
@@ -1,5 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = client => {
-    console.log(`you have been disconected at ${new Date()}`);
-}
--- events/reconnecting.js
+++
@@ -1,5 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports = client => {
-    console.log(`you have been reconnecting at ${new Date()}`);
-}
